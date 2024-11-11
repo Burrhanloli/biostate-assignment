14060fdce5e9f6831d682222c7f425b217e17730
burhanevive <burhanuddin.l@goevive.com>
2024-11-11 19:59:41 +0530

    made DATABASE_URL optional

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -15,7 +15,7 @@ export const env = createEnv({
     DB_PASSWORD: z.string(),
     DB_NAME: z.string(),
     DB_PORT: z.coerce.number(),
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url().optional(),
     DB_MIGRATING: z
       .string()
       .refine((s) => s === "true" || s === "false")
